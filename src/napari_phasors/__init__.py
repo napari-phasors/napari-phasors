192a42fec483a90223a31909c2ae09ffaa11105d
bruno-pannunzio <brunopannunzio@gmail.com>
2024-07-19 11:48:11 -0300

    Update `_reader.py` and added reader widget in `_widget.py`. Still tests are missing.

--- src/napari_phasors/__init__.py
+++ src/napari_phasors/__init__.py
@@ -3,10 +3,6 @@ __version__ = "0.0.1"
 from ._reader import napari_get_reader
 from ._sample_data import make_sample_data
 from ._widget import (
-    ExampleQWidget,
-    ImageThreshold,
-    threshold_autogenerate_widget,
-    threshold_magic_widget,
     PhasorTransform,
 )
 from .plotter import PlotterWidget
@@ -17,9 +13,5 @@ __all__ = (
     "napari_get_reader",
     "write_ome_tiff",
     "make_sample_data",
-    "ExampleQWidget",
-    "ImageThreshold",
-    "threshold_autogenerate_widget",
-    "threshold_magic_widget",
     "PhasorTransform",
 )
--- src/napari_phasors/_reader.py
+++ src/napari_phasors/_reader.py
@@ -3,31 +3,49 @@ This module contains functions to read files supported by `phasorpy.io`
 and computes phasor coordinates with `phasorpy.phasor.phasor_from_signal`
 
 """
-import os
-import numpy as np
-import phasorpy.io as io
-from phasorpy.phasor import phasor_from_signal
-import pandas as pd
-from napari.layers import Labels
+
 import inspect
+import os
+from typing import Any, Callable, Optional
+
+import numpy as np
+import pandas as pd
+import phasorpy.io as io
+from napari.layers import Labels
+from napari.utils.notifications import show_error
+from phasorpy.phasor import phasor_from_signal
 
 extension_mapping = {
-    'raw': {
-        ".ptu": lambda path, options: _parse_and_call_io_function(path, io.read_ptu, {"frame": (-1, False), "keepdims": (False, False)}, options),
-        ".fbd": lambda path, options: _parse_and_call_io_function(path, io.read_fbd, {"frame": (-1, False), "keepdims": (False, False)}, options),
-        ".lsm": lambda path, options: _parse_and_call_io_function(path, io.read_lsm, {}, options),
+    "raw": {
+        ".ptu": lambda path, options: _parse_and_call_io_function(
+            path,
+            io.read_ptu,
+            {"frame": (-1, False), "keepdims": (True, False)},
+            options,
+        ),
+        ".fbd": lambda path, options: _parse_and_call_io_function(
+            path,
+            io.read_fbd,
+            {"frame": (-1, False), "keepdims": (True, False)},
+            options,
+        ),
+        ".lsm": lambda path, options: _parse_and_call_io_function(
+            path, io.read_lsm, {}, options
+        ),
         # ".flif": lambda path: io.read_flif(path),
         # ".sdt": lambda path: io.read_sdt(path),
         # ".bh": lambda path: io.read_bh(path),
         # ".bhz": lambda path: io.read_bhz(path),
         # ".ifli": lambda path: io.read_ifli(),
     },
-    'processed': {
-        ".tif": lambda path, options: _parse_and_call_io_function(path, io.read_ometiff_phasor, {}, options),
+    "processed": {
+        ".tif": lambda path, options: _parse_and_call_io_function(
+            path, io.read_ometiff_phasor, {}, options
+        ),
         # ".b64": lambda path: io.read_b64(path),
         # ".r64": lambda path: io.read_r64(path),
         # ".ref": lambda path: io.read_ref(path)
-    }
+    },
 }
 """This dictionary contains the mapping for reader functions from
 `phasorpy.io` supported formats.
@@ -36,27 +54,28 @@ Commented file extensions are not supported at the moment.
 
 """
 
-iter_index_mapping = {
-    ".ptu": 'C',
-    ".lsm": None,
-    ".fbd": 'C'
-}
+iter_index_mapping = {".ptu": "C", ".fbd": "C", ".lsm": None}
 """This dictionary contains the mapping for the axis to iterate over
 when calculating phasor coordinates in the file.
 """
 
-def napari_get_reader(path: str = None, options: dict = None):
+
+def napari_get_reader(
+    path: str, options: Optional[dict] = None
+) -> Optional[Callable]:
     """Initial reader function to map file extension to
     specific reader functions.
-    
+
     Parameters
     ----------
     path : str
         Path to file.
-        
+    options : dict, optional
+        Dictionary containing the arguments to pass to the function.
+
     Returns
     -------
-    layer_data : list of tuples
+    layer_data : list of tuples, or None
         A list of LayerData tuples where each tuple in the list contains a
         napari.layers.Labels layer a tuple  (data, kwargs), where data is
         the mean intensity image as an array, and kwargs is a a dict of
@@ -64,45 +83,18 @@ def napari_get_reader(path: str = None, options: dict = None):
         which contains the 'name' of the layer as well as the 'metadata',
         which is also a dict. The values for key 'phasor_features_labels_layer'
         in 'metadata' contain phasor coordinates as columns 'G' and 'S'.
-    
+
     """
     _, file_extension = os.path.splitext(path)
     file_extension = file_extension.lower()
-    if file_extension in extension_mapping['processed'].keys():
-        return lambda  path: processed_file_reader(path, options=options)
-    elif file_extension in extension_mapping['raw'].keys():
+    if file_extension in extension_mapping["processed"].keys():
+        return lambda path: processed_file_reader(path, options=options)
+    elif file_extension in extension_mapping["raw"].keys():
         return lambda path: raw_file_reader(path, options=options)
     else:
-        return unkonwn_reader_function
+        show_error('File extension not supported.')
 
-def unkonwn_reader_function(path: str | list[str]):
-    """Generalized reader function used for formats not supported.
-
-    Parameters
-    ----------
-    path : str or list of str
-        Path to file, or list of paths.
-
-    Returns
-    -------
-    layer_data : list of tuples
-        A list of LayerData tuples where each tuple in the list contains
-        (data, metadata, layer_type), where data is a numpy array, metadata is
-        a dict of keyword arguments for the corresponding viewer.add_* method
-        in napari, and layer_type is a lower-case string naming the type of
-        layer. Both "meta", and "layer_type" are optional. napari will
-        default to layer_type=="image" if not provided
-
-    """
-    paths = [path] if isinstance(path, str) else path
-    arrays = [np.load(_path) for _path in paths]
-    data = np.squeeze(np.stack(arrays))
-    add_kwargs = {}
-    layer_type = "image"
-    return [(data[0], add_kwargs, layer_type)]
-
-
-def raw_file_reader(path: str = None, options = None):
+def raw_file_reader(path: str, options: Optional[dict] = None) -> list[tuple]:
     """Read raw data files from supported file formats and apply the phasor
     transformation to get mean intensity image and phasor coordinates.
 
@@ -124,26 +116,44 @@ def raw_file_reader(path: str = None, options = None):
 
     """
     filename, file_extension = _get_filename_extension(path)
-    raw_data = extension_mapping['raw'][file_extension](path, options)
+    raw_data = extension_mapping["raw"][file_extension](path, options)
     layers = []
     iter_axis = iter_index_mapping[file_extension]
     if iter_axis is None:
         # Calculate phasor over channels if file is of hyperspectral type
-        mean_intensity_image, G_image, S_image = phasor_from_signal(raw_data, axis=raw_data.dims.index('C'))
-        labels_layer = make_phasors_labels_layer(mean_intensity_image, G_image, S_image, name=filename)
-        add_kwargs = {'name': f'{filename} Intensity Image', 'metadata':{'phasor_features_labels_layer': labels_layer}}
+        mean_intensity_image, G_image, S_image = phasor_from_signal(
+            raw_data, axis=raw_data.dims.index("C")
+        )
+        labels_layer = make_phasors_labels_layer(
+            mean_intensity_image, G_image, S_image, name=filename
+        )
+        add_kwargs = {
+            "name": f"{filename} Intensity Image",
+            "metadata": {"phasor_features_labels_layer": labels_layer},
+        }
         layers.append((mean_intensity_image, add_kwargs))
     else:
         iter_axis_index = raw_data.dims.index(iter_axis)
         for channel in range(raw_data.shape[iter_axis_index]):
-            # Calculate phasor over photon counts dimension if file is of FLIM type
-            mean_intensity_image, G_image, S_image = phasor_from_signal(raw_data.sel(C=channel), axis=raw_data.sel(C=channel).dims.index('H'))
-            labels_layer = make_phasors_labels_layer(mean_intensity_image, G_image, S_image, name=filename)
-            add_kwargs = {'name': f'{filename} Intensity Image: Channel {channel}', 'metadata':{'phasor_features_labels_layer': labels_layer}}
+            # Calculate phasor over photon counts dimension if file is FLIM
+            mean_intensity_image, G_image, S_image = phasor_from_signal(
+                raw_data.sel(C=channel),
+                axis=raw_data.sel(C=channel).dims.index("H"),
+            )
+            labels_layer = make_phasors_labels_layer(
+                mean_intensity_image, G_image, S_image, name=filename
+            )
+            add_kwargs = {
+                "name": f"{filename} Intensity Image: Channel {channel}",
+                "metadata": {"phasor_features_labels_layer": labels_layer},
+            }
             layers.append((mean_intensity_image, add_kwargs))
     return layers
 
-def processed_file_reader(path: str = None, options: dict = None):
+
+def processed_file_reader(
+    path: str, options: Optional[dict[str, str]] = None
+) -> list[tuple]:
     """Reader function for files that contain processed images, as phasor
     coordinates or intensity images.
 
@@ -162,18 +172,32 @@ def processed_file_reader(path: str = None, options: dict = None):
         which contains the 'name' of the layer as well as the 'metadata',
         which is also a dict. The values for key 'phasor_features_labels_layer'
         in 'metadata' contain phasor coordinates as columns 'G' and 'S'.
- 
+
     """
     filename, file_extension = _get_filename_extension(path)
-    mean_intensity_image, G_image, S_image = extension_mapping['processed'][file_extension](path)
-    mean_intensity_image, G_image, S_image = mean_intensity_image.values, G_image.values, S_image.values
-    labels_layer = make_phasors_labels_layer(mean_intensity_image, G_image, S_image, name=filename)
+    mean_intensity_image, G_image, S_image = extension_mapping["processed"][
+        file_extension
+    ](path)
+    mean_intensity_image, G_image, S_image = (
+        mean_intensity_image.values,
+        G_image.values,
+        S_image.values,
+    )
+    labels_layer = make_phasors_labels_layer(
+        mean_intensity_image, G_image, S_image, name=filename
+    )
     layers = []
-    add_kwargs = {'name': filename + ' Intensity Image', 'metadata':{'phasor_features_labels_layer': labels_layer}}
+    add_kwargs = {
+        "name": filename + " Intensity Image",
+        "metadata": {"phasor_features_labels_layer": labels_layer},
+    }
     layers.append((mean_intensity_image, add_kwargs))
     return layers
 
-def make_phasors_labels_layer(mean_intensity_image, G_image, S_image, name=''):
+
+def make_phasors_labels_layer(
+    mean_intensity_image: Any, G_image: Any, S_image: Any, name: str = ""
+) -> Labels:
     """Create a napari Labels layer from phasor coordinates.
 
     Parameters
@@ -197,30 +221,59 @@ def make_phasors_labels_layer(mean_intensity_image, G_image, S_image, name=''):
     if len(G_image.shape) > 2:
         table = pd.DataFrame([])
         for i in range(G_image.shape[0]):
-            sub_table = pd.DataFrame({'label': pixel_id, 'G': G_image[i].ravel(), 'S': S_image[i].ravel(), 'harmonic': i+1})  
+            sub_table = pd.DataFrame(
+                {
+                    "label": pixel_id,
+                    "G": G_image[i].ravel(),
+                    "S": S_image[i].ravel(),
+                    "harmonic": i + 1,
+                }
+            )
             table = pd.concat([table, sub_table])
     else:
-        table = pd.DataFrame({'label': pixel_id, 'G': G_image.ravel(), 'S': S_image.ravel(), 'harmonic': 1})
+        table = pd.DataFrame(
+            {
+                "label": pixel_id,
+                "G": G_image.ravel(),
+                "S": S_image.ravel(),
+                "harmonic": 1,
+            }
+        )
     labels_data = pixel_id.reshape(mean_intensity_image.shape)
-    labels_layer = Labels(labels_data, name=name + ' Phasor Features Layer', scale=(1, 1), features=table)
+    labels_layer = Labels(
+        labels_data,
+        name=name + " Phasor Features Layer",
+        scale=(1, 1),
+        features=table,
+    )
     return labels_layer
 
 
-def _parse_and_call_io_function(path, func, args_defaults, options=None):
-    """
-    A private helper function to parse arguments and call the specified io function.
+def _parse_and_call_io_function(
+    path: str,
+    func: Callable,
+    args_defaults: dict[str, Any],
+    options: Optional[dict[str, Any]] = None,
+) -> Any:
+    """Private helper function to parse arguments and call a `io` function.
 
-    Parameters:
-    - path (str): The file path to be passed to the io function.
-    - func (callable): The io function to be called.
-    - args_defaults (dict): A dictionary where keys are argument names and values are tuples of
-                            (default_value, is_required). If is_required is True and the argument
-                            is not present in the args_defaults, an exception is raised.
-    - options (dict, optional): A dictionary of arguments that may override the defaults and
-                                provide additional arguments not specified in args_defaults.
+    Parameters
+    ----------
+    path : str
+        Path to file.
+    func : callable
+        Function to call.
+    args_defaults : dict
+        Dictionary containing the default arguments for the function.
+    options : dict, optional
+        Dictionary containing the arguments to pass to the function.
+        Default is None.
 
-    Returns:
-    The result of calling the io function with the parsed arguments.
+    Returns
+    -------
+    data : xarray.DataArray
+        Data read from the file
+
     """
     args = {}
     # Use options if provided, otherwise use the default
@@ -242,12 +295,28 @@ def _parse_and_call_io_function(path, func, args_defaults, options=None):
         if arg in sig.parameters:
             valid_args[arg] = value
         else:
-            raise ValueError(f"Invalid argument '{arg}' for function {func.__name__}.")
-    print(valid_args)
+            raise ValueError(
+                f"Invalid argument '{arg}' for function {func.__name__}."
+            )
     return func(path, **valid_args)
 
-def _get_filename_extension(path: str):
-    """Get the filename and extension from a path."""
+
+def _get_filename_extension(path: str) -> tuple[str, str]:
+    """Get the filename and extension from a path.
+
+    Parameters
+    ----------
+    path : str
+        Path to file.
+
+    Returns
+    -------
+    filename : str
+        Filename.
+    file_extension : str
+        File extension.
+
+    """
     filename = os.path.basename(path)
     _, file_extension = os.path.splitext(filename)
     return filename, file_extension.lower()
