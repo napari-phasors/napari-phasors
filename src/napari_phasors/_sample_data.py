737bb2695c072f11abfd8fd96cee75392ddecbd6
bruno-pannunzio <brunopannunzio@gmail.com>
2024-11-16 11:34:37 -0300

    Add covallaria fbd dataset.

--- src/napari_phasors/_sample_data.py
+++ src/napari_phasors/_sample_data.py
@@ -9,14 +9,14 @@ Replace code below according to your needs.
 
 from __future__ import annotations
 
-import numpy
+import pooch
+from napari_phasors._reader import napari_get_reader
 
 
-def make_sample_data():
-    """Generates an image"""
-    # Return list of tuples
-    # [(data1, add_image_kwargs1), (data2, add_image_kwargs2)]
-    # Check the documentation for more information about the
-    # add_image_kwargs
-    # https://napari.org/stable/api/napari.Viewer.html#napari.Viewer.add_image
-    return [(numpy.random.rand(512, 512), {})]
+def convallaria_FLIM_sample_data():
+    """Fetch Convallaria image and Calibration"""
+    data = pooch.retrieve('https://zenodo.org/records/14026720/files/Convallaria_$EI0S.fbd?download=1', known_hash='3751891b02e3095fedd53a09688d8a22ff2a0083544dd5c0726b9267d11df1bc')
+    calibration_data = pooch.retrieve('https://zenodo.org/records/14026720/files/Calibration_Rhodamine110_$EI0S.fbd?download=1', known_hash='d745cbcdd4a10dbaed83ee9f1b150f0c7ddd313031e18233293582cdf10e4691')
+    reader_options = {'channel': 0}
+    reader = napari_get_reader(data, reader_options=reader_options)
+    return [reader(data)[0], reader(calibration_data)[0]]
