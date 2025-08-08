dbfc8d02eeb76afc671f8c402fd31c1cf4c10c19
Marcelo Zoccoler <marcelozoc@gmail.com>
2025-08-08 10:29:29 +0200

    Use dynamic version import with fallback

--- src/napari_phasors/__init__.py
+++ src/napari_phasors/__init__.py
@@ -1,4 +1,7 @@
-__version__ = "0.0.5"
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
 
 from ._reader import napari_get_reader
 from ._sample_data import (
