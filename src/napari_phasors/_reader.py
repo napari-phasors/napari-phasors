4edd39c2538c940e43cbb83ab5652f42fe6da48e
Marcelo Zoccoler <marcelozoc@gmail.com>
2025-09-03 15:46:12 +0200

    Add 'mask' column to phasors labels table
    A 'mask' column with value 1 is now added to the Labels layer features in `make_phasors_labels_layer`.

--- src/napari_phasors/_reader.py
+++ src/napari_phasors/_reader.py
@@ -434,6 +434,7 @@ def make_phasors_labels_layer(
                     "harmonic": harmonic_value,
                 }
             )
+            sub_table['mask'] = 1
             table = pd.concat([table, sub_table])
     else:
         if isinstance(harmonics, list):
@@ -450,6 +451,7 @@ def make_phasors_labels_layer(
                 "harmonic": harmonic_value,
             }
         )
+        table['mask'] = 1
 
     labels_data = pixel_id.reshape(mean_intensity_image.shape)
     labels_layer = Labels(
