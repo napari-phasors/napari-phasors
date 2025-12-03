c36d51d2f31eebf38fe16089fc58e7f47a6c2360
bruno-pannunzio <brunopannunzio@gmail.com>
2025-12-03 14:00:26 -0300

    Update writer widget test for updated info shown.

--- src/napari_phasors/_tests/test_widget.py
+++ src/napari_phasors/_tests/test_widget.py
@@ -677,7 +677,7 @@ def test_writer_widget(make_napari_viewer, tmp_path):
     with patch("napari_phasors._widget.show_error") as mock_show_error:
         main_widget.search_button.click()
         mock_show_error.assert_called_once_with(
-            "No layer with phasor data selected"
+            "No layer selected"
         )
     # Create a synthetic FLIM data and an intensity image layer with phasors
     raw_flim_data = make_raw_flim_data()
